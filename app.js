fe5779a4ffc59f008c2ebcf7222fe66a2c8134d4
Nadia <elhe2013@gmail.com>
2021-08-10 11:43:58 +0300

    add

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const authRouter = require("./routes/auth.routes");
 const linkRouter = require("./routes/link.router");
 const redirectRouter = require("./routes/redirect.routes");
-require('dotenv').config()
+require("dotenv").config();
 const app = express();
 
 const PORT = process.env.PORT || 3002;
@@ -17,23 +17,25 @@ app.use("/api/link", linkRouter);
 app.use("/t", redirectRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "react_repeat", "build")));
+  app.use("/", express.static(path.join(__dirname, "build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "react_repeat", "build", "index.html")
-    );
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
   });
 }
 console.log("process.env.MONGO_URI", process.env.MONGO_URI);
 
 async function start() {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-    }, ()=> console.log(`DB connect....`));
+    await mongoose.connect(
+      process.env.MONGO_URI,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+      },
+      () => console.log(`DB connect....`)
+    );
     app.listen(PORT, () => {
       console.log(`App hes been started on port ${PORT}....`);
     });
